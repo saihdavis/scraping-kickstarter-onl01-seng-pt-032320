4dc01b92142fc7f0071ebde4fccffba0159e89d5
Saida Davis <saihdavis@gmail.com>
2020-05-08 23:35:57 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,9 @@
+# file: kickstarter_scraper.rb
+
 require "nokogiri"
 require "pry"
+
+
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
@@ -14,3 +18,5 @@ create_project_hash
 # title: project.css("h2.bbcard_name strong a").text
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
+# location: project.css("ul.project-meta span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
